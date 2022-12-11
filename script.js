9a881d25c792a3cb0e22b14a188473cc0fe14c98
Totpoter <nsbdev@gmail.com>
2022-12-11 21:36:09 +0100

    fixed unneccessary stuff and added drop down menu to account

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const language_icon = language.querySelector("span");
 const burger_icon = burger.querySelector("span");
 
 const mobile_menu = document.querySelector(".mobile-menu");
+const account_menu = document.querySelector(".account-menu");
 
 burger.addEventListener("click", () => {
     burger_icon.innerText = burger_icon.innerText === "menu" 
@@ -17,3 +18,7 @@ burger.addEventListener("click", () => {
 
     mobile_menu.classList.toggle("is-open");
 }) /* If the span text is equal to menu, change it to close, otherwise change it to menu so the icon changes appropriately using google icons*/
+
+account.addEventListener("click", () => {
+    account_menu.classList.toggle("is-open");
+})
